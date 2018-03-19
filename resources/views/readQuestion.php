492da68310ffd391270dff9d081a609f32eb1efc
not-the-bees <kirua147@hotmail.com>
2018-03-19 17:44:59 +0100

    avancement page readQuestion

--- resources/views/readQuestion.php
+++ resources/views/readQuestion.php
@@ -9,28 +9,30 @@
     <link rel="stylesheet" type="text/css" href="../public/css/mdb.min.css">
     <link rel="stylesheet" type="text/css" href="../public/css/fontawesome-all.css">
     <link rel="stylesheet" type="text/css" href="../public/css/style.css">
-    <link rel="stylesheet" type="text/css" href="../public/css/form.css">
     <title>TalkAboutStuff - <?= ucfirst($question['title']); ?></title>
 </head>
 
 <!-- Banner -->
 <body>
 <header class="banner">
-    <h1 class="title"><?= ucfirst($question['title']); ?></h1>
+    <h1 class="title">Talk About Stuff - Browse Questions</h1>
 </header>
 <!-- End Banner -->
+
+<br>
 <section class="container">
     <section class="row">
-        <div class="text-center">
-        <div class="card text-center">
-            <h2 class="card-header"><?= ucfirst($question['content']); ?></h2>
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="col-md-4 offset-md-4">
+            <div class="card questionCard">
+                <h2 class="card-header"><?= ucfirst($question['title']); ?></h2>
+                <div class="card-body">
+                    <p class="card-text"><?= ucfirst($question['content']); ?></p>
+                    <a href="" class="btn btn-primary btn-large btn-deep-orange boutonHome m-0">Answer</a>
+                    <a href="" class="btn btn-primary btn-large btn-deep-orange boutonHome m-0">Leave a comment</a>
+                </div>
             </div>
         </div>
-        </div>
+        <div class="col-md-4 offset-md-4"></div>
     </section>
 </section>
 
