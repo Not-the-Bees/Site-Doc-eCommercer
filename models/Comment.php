a9790601ed7067e48edbdebc7f6b06c94f286fea
not-the-bees <kirua147@hotmail.com>
2018-03-26 16:53:41 +0200

    debut developpement partie commentaire

--- models/Comment.php
+++ models/Comment.php
@@ -1,11 +1,17 @@
 <?php
 class Comment
 {
-    //Ajout d'un commentaire sur une question
+    /**
+     * Add a comment to a specific question
+     * @param $question_id
+     * @param $content
+     * @param $user_id
+     * @return null|PDOStatement
+     */
     function addQuestionComment($question_id, $content, $user_id)
     {
 
-        $pdo_statement = prepareStatement(
+        $pdo_statement = Database::prepareStatement(
             'INSERT INTO question_comment (question_id, content, user_id) VALUES (:question_id, :content, :user_id)');
 
         if (
@@ -19,11 +25,17 @@ class Comment
         }
     }
 
-    //Ajout d'un commentaire sur une réponse
+    /**
+     * Add a comment to a specific answer
+     * @param $answer_id
+     * @param $content
+     * @param $user_id
+     * @return null|PDOStatement
+     */
     function addAnswerComment($answer_id, $content, $user_id)
     {
 
-        $pdo_statement = prepareStatement(
+        $pdo_statement = Database::prepareStatement(
             'INSERT INTO answer_comment (answer_id, content, user_id) VALUES (:answer_id, :content, :user_id)');
 
         if (
