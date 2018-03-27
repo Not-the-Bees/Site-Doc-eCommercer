0a4d3b5352944df6741fbea9ac3d01836cbc4a10
not-the-bees <kirua147@hotmail.com>
2018-03-27 08:18:36 +0200

    création méthodes commentaire question + réponse

--- models/Comment.php
+++ models/Comment.php
@@ -1,51 +1,52 @@
 <?php
+
 class Comment
 {
     /**
-     * Add a comment to a specific question
      * @param $question_id
      * @param $content
      * @param $user_id
-     * @return null|PDOStatement
+     * @return bool
      */
-    function addQuestionComment($question_id, $content, $user_id)
+    static function addCommentToQuestion($question_id, $content, $user_id)
     {
 
         $pdo_statement = Database::prepareStatement(
             'INSERT INTO question_comment (question_id, content, user_id) VALUES (:question_id, :content, :user_id)');
 
-        if (
-            $pdo_statement &&
-            $pdo_statement->bindParam(':question_id', $question_id) &&
-            $pdo_statement->bindParam(':content', $content) &&
-            $pdo_statement->bindParam(':user_id', $user_id) &&
-            $pdo_statement->execute()
-        ) {
-            return $pdo_statement;
+        if ($pdo_statement)
+        {
+            return $pdo_statement->execute(array(
+                ':question_id'=>$question_id,
+                ':content'=>$content,
+                ':user_id'=>$user_id
+            ));
+        } else {
+            return false;
         }
     }
 
     /**
-     * Add a comment to a specific answer
      * @param $answer_id
      * @param $content
      * @param $user_id
-     * @return null|PDOStatement
+     * @return bool
      */
-    function addAnswerComment($answer_id, $content, $user_id)
+    static function addCommentToAnswer($answer_id, $content, $user_id)
     {
 
         $pdo_statement = Database::prepareStatement(
             'INSERT INTO answer_comment (answer_id, content, user_id) VALUES (:answer_id, :content, :user_id)');
 
-        if (
-            $pdo_statement &&
-            $pdo_statement->bindParam(':answer_id', $answer_id) &&
-            $pdo_statement->bindParam(':content', $content) &&
-            $pdo_statement->bindParam(':user_id', $user_id) &&
-            $pdo_statement->execute()
-        ) {
-            return $pdo_statement;
+        if ($pdo_statement)
+        {
+            return $pdo_statement->execute(array(
+                ':answer_id'=>$answer_id,
+                ':content'=>$content,
+                ':user_id'=>$user_id
+            ));
+        } else {
+            return false;
         }
     }
 }
